d253cff718f1cfc71c8507b178d8fe483000b2da
Drew Jaynes <xoodrew@gmail.com>
2024-10-12 09:44:25 -0600

    Refactor panel to add escaping, standardize translations, and clean up markup output.

--- src/Panel.php
+++ src/Panel.php
@@ -18,7 +18,7 @@ class Panel extends \Debug_Bar_Panel
 	 * {@inheritDoc}
 	 */
 	function init() {
-		$this->title( 'Console' );
+		$this->title(__('Console', 'debug-bar-console-reloaded'));
 
 		add_action('wp_ajax_debug_bar_console', [&$this, 'ajaxCallback']);
 	}
@@ -40,7 +40,6 @@ class Panel extends \Debug_Bar_Panel
 		);
 
 		$mode = 'php';
-
 		?>
 		<form id="debug-bar-console" class="debug-bar-console-mode-<?php echo esc_attr($mode); ?>">
 		<input id="debug-bar-console-iframe-css" type="hidden" value="<?php echo plugins_url('assets/css/iframe.dev.css', DebugBarConsoleReloaded::FILE); ?>" />
@@ -49,33 +48,36 @@ class Panel extends \Debug_Bar_Panel
 			<ul class="debug-bar-console-tabs">
 				<?php foreach ( $modes as $slug => $title ):
 					$classes = 'debug-bar-console-tab';
-					if ( $slug == $mode )
+					if ( $slug == $mode ) {
 						$classes .= ' debug-bar-console-tab-active';
+					}
 					?>
-					<li class="<?php echo esc_attr( $classes ); ?>">
-						<?php echo $title; ?>
-						<input type="hidden" value="<?php echo $slug; ?>" />
+					<li class="<?php echo esc_attr($classes); ?>">
+						<?php echo esc_html($title); ?>
+						<input type="hidden" value="<?php echo esc_attr($slug); ?>" />
 					</li>
 				<?php endforeach; ?>
 			</ul>
 			<div id="debug-bar-console-submit">
-				<span><?php _e('Shift + Enter'); ?></span>
-				<a href="#"><?php _e('Run'); ?></a>
+				<span><?php _e('Shift + Enter', 'debug-bar-console-reloaded'); ?></span>
+				<a href="#"><?php esc_html_e('Run', 'debug-bar-console-reloaded'); ?></a>
 			</div>
 			<div class="debug-bar-console-panel debug-bar-console-on-php">
+				<label for="debug-bar-console-input-php" class="screen-reader-text"><?php esc_html_e('Enter PHP code to execute.', 'debug-bar-console-reloaded'); ?></label>
 				<textarea id="debug-bar-console-input-php" class="debug-bar-console-input"><?php echo "<?php\n"; ?></textarea>
 			</div>
 			<div class="debug-bar-console-panel debug-bar-console-on-sql">
+				<label for="debug-bar-console-input-sql" class="screen-reader-text"><?php esc_html_e('Enter SQL to execute.', 'debug-bar-console-reloaded'); ?></label>
 				<textarea id="debug-bar-console-input-sql" class="debug-bar-console-input"></textarea>
 			</div>
 		</div>
 		<div id="debug-bar-console-output">
 			<ul class="debug-bar-console-tabs">
-				<li class="debug-bar-console-tab debug-bar-console-tab-active" data-output-mode="formatted"><?php _e('Formatted'); ?></li>
-				<li class="debug-bar-console-tab" data-output-mode="raw"><?php _e('Raw'); ?></li>
+				<li class="debug-bar-console-tab debug-bar-console-tab-active" data-output-mode="formatted"><?php esc_html_e('Formatted', 'debug-bar-console-reloaded'); ?></li>
+				<li class="debug-bar-console-tab" data-output-mode="raw"><?php esc_html_e('Raw', 'debug-bar-console-reloaded'); ?></li>
 			</ul>
 			<div class="debug-bar-console-panel">
-				<iframe></iframe>
+				<iframe title="<?php esc_attr_e('Output', 'debug-bar-console-reloaded'); ?>"></iframe>
 			</div>
 		</div>
 		</form>
@@ -92,66 +94,82 @@ class Panel extends \Debug_Bar_Panel
 	function ajaxCallback() {
 		global $wpdb;
 
-		if ( false === check_ajax_referer( 'Debug_Bar_Console_Reloaded', 'nonce', false ) )
+		if ( false === check_ajax_referer('Debug_Bar_Console_Reloaded', 'nonce', false)) {
 			die();
+		}
 
-		if ( ! is_super_admin() || ! isset( $_POST['mode'] ) )
+		if (!is_super_admin()) {
 			die();
+		}
 
-		$data = stripslashes( $_POST['data'] );
-		$mode = $_POST['mode'];
+		$data = stripslashes($_REQUEST['data'] ?? '');
+		$mode = $_REQUEST['mode'] ?? 'php';
 
-		if ( 'php' == $mode ) {
+
+		if ( 'php' === $mode ) {
 			// Trim the data
-			$data = '?>' . trim( $data );
+			$data = '?>' . trim($data);
 
 			// Do we end the string in PHP?
-			$open  = strrpos( $data, '<?php' );
-			$close = strrpos( $data, '?>' );
+			$open  = strrpos($data, '<?php');
+			$close = strrpos($data, '?>');
 
 			// If we're still in PHP, ensure we end with a semicolon.
-			if ( $open > $close )
-				$data = rtrim( $data, ';' ) . ';';
+			if ($open > $close) {
+				$data = rtrim($data, ';') . ';';
+			}
 
-			eval( $data );
+			eval($data);
 			die();
 
-		} elseif ( 'sql' == $mode ) {
-			$data = explode( ";\n", $data );
-			foreach ( $data as $query ) {
-				$query = str_replace( '$wpdb->', $wpdb->prefix, $query );
-				$this->print_mysql_table( $wpdb->get_results( $query, ARRAY_A ), $query );
+		} elseif ('sql' === $mode) {
+			$data = explode(";\n", $data);
+			foreach ($data as $query ) {
+				$query = str_replace('$wpdb->', $wpdb->prefix, $query);
+				$this->printMysqlTable($wpdb->get_results($query, ARRAY_A), $query);
 			}
 			die();
 		}
 	}
 
-	function print_mysql_table( $data, $query='' ) {
-		if ( empty( $data ) )
+	/**
+	 * Prints the MySQL table.
+	 *
+	 * @param array<mixed> $data Found rows.
+	 * @param string $query Optional. Query. Default empty string.
+	 * @return void
+	 */
+	function printMysqlTable( $data, $query = '')
+	{
+		if ( empty($data) )
 			return;
 
 		$keys = array_keys( $data[0] );
 
-		echo '<table class="mysql" cellpadding="0"><thead>';
-
-		if ( ! empty( $query ) )
-			echo "<tr class='query'><td colspan='" . count($keys) . "'>$query</td></tr>";
-
-		echo '<tr>';
-		foreach ( $keys as $key ) {
-			echo "<th class='$key'>$key</th>";
-		}
-		echo '</tr></thead><tbody>';
-
-		foreach ( $data as $row ) {
-			echo '<tr>';
-			foreach ( $row as $key => $value ) {
-				echo "<td class='$key'>" . esc_html($value) . "</td>";
-			}
-			echo '</tr>';
-		}
-
-		echo '</tbody></table></div>';
+		?>
+		<table class="mysql" cellpadding="0">
+			<thead>
+				<tr class="query">
+					<td colspan="<?php echo esc_attr(count($keys)); ?>"><?php echo $query; ?></td>
+				</tr>
+				<tr>
+					<?php foreach ($keys as $key): ?>
+					<th class="<?php echo esc_attr($key); ?>"><?php echo esc_html($key); ?></th>
+					<?php endforeach; ?>
+				</tr>
+			</thead>
+			<tbody>
+				<?php foreach ($data as $row): ?>
+				<tr>
+					<?php foreach ($row as $key => $value): ?>
+					<td class="<?php echo esc_attr($key); ?>"><?php echo esc_html($value); ?></td>
+					<?php endforeach; ?>
+				</tr>
+				<?php endforeach; ?>
+			</tbody>
+		</table>
+		</div>
+		<?php
 	}
 }
 
